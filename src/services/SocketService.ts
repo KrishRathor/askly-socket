a4ca0e0712df5d812b7a92bb5bd20bf483831bac
Krish Rathor <krishrathor18@gmail.com>
2024-10-18 12:57:47 +0530

    setup socket server

---
+++ src/services/SocketService.ts
@@ -0,0 +1,29 @@
+import { Server } from "socket.io";
+
+export class SocketService {
+    private _io: Server;
+  
+    constructor() {
+      console.log(`Init Socket Service`);
+      this._io = new Server({
+        cors: {
+          allowedHeaders: ['*'],
+          origin: '*'
+        }
+      });
+    }
+
+    get io() {
+        return this._io;
+    }
+
+    public initListeners() {
+        const io = this.io;
+    
+        console.log(`Initialized Socket Listeners`);
+    
+        io.on('connect', (socket) => {
+          console.log(`New socket connected... with socket id ${socket.id}`)
+        })
+    }
+}
